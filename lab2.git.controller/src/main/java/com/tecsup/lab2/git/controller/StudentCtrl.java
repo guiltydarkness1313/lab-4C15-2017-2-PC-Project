996145c4171d6639d681f3d60e901edbd1d359c5
Paul Frank <paul.frankpc@gmail.com>
2017-08-22 11:53:22 -0500

    sda

--- lab2.git.controller/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl.java
+++ lab2.git.controller/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl.java
@@ -8,6 +8,6 @@ public class StudentCtrl
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello PACMAN!" );
     }
 }
