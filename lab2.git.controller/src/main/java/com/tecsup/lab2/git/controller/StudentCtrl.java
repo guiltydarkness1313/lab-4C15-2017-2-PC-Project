b628b0d6f8c83bb722c1f30b44cd5a78ae2495d1
Paul Frank <paul.frankpc@gmail.com>
2017-08-22 10:37:45 -0500

    initial commit

---
+++ lab2.git.controller/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab2.git.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class StudentCtrl 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lab2.git.controller/src/test/java/com/tecsup/lab2/git/controller/StudentCtrlTest.java
@@ -0,0 +1,38 @@
+package com.tecsup.lab2.git.controller;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class StudentCtrlTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public StudentCtrlTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( StudentCtrlTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
